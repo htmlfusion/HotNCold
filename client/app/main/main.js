371c687173b0482ab9f287084c1e992296aeecf4
Philippe Lewicki <philippe@htmlfusion.com>
2015-04-01 16:44:24 -0700

    modified express started intro

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -4,8 +4,6 @@ angular.module('gishAppApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('main', {
-        url: '/main',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        url: '/main'
       });
   });
