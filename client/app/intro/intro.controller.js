8db2ac5a53df046ae16b5f171f1b6865c4fa5d2e
Philippe Lewicki <philippe@htmlfusion.com>
2015-04-03 16:18:05 -0700

    Binh changes on the carousel

--- client/app/intro/intro.controller.js
+++ client/app/intro/intro.controller.js
@@ -2,12 +2,57 @@
 
 angular.module('gishAppApp')
   .controller('IntroCtrl', function ($scope, $timeout) {
-    $scope.intros = [
-      {title: "Welcome"},
-      {title: "You're going to have to move your feet", link: "gradient"},
-      {title: "The closer you get, the hotter you get", link: "gradient"},
-      {title: "The further you get, the colder you get", link: "gradient"},
-      {title: "Start moving!", link: "gradient"},
-    ];
+    $scope.intros = [{
+        /*caption: "Welcome"
+      },{
+        caption: "You're going to have to move your feet"
+      },{
+        caption: "The closer you get, the hotter you get"
+      },{
+        caption: "The further you get, the colder you get"
+      },{
+        caption: "Start moving!"
+      },{
+        caption: ""
+      },{
+        caption: {
+          earned: "You've earned the",
+          badge: "Explorer's Badge",
+          src: "app/intro/explorer-badge.png",
+          prize: "Found the prize",
+          points: "9000+ points",
+          replay: "Replay"   
+      }*/
+      
+      
+      
+      
+      
+      
+      caption: "Welcome"
+      },{
+        caption2: "You're going to have to move your feet"
+      },{
+        caption3: "The closer you get, the hotter you get"
+      },{
+        caption4: "The further you get, the colder you get"
+      },{
+        caption5: "Start moving!"
+      },{
+        caption6: ""
+      },{
+        caption7: {
+          earned: "You've earned the",
+          badge: "Explorer's Badge",
+          src: "app/intro/explorer-badge.png",
+          prize: "Found the prize",
+          points: "9000+ points",
+          replay: "Replay"   
+      }
+    }];
+    
+    
     $scope.user = {name: ''};
   });
+  
+
