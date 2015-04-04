b7816c791f2bf6781e85e170a5c81056926eb096
Jesse Vander Does <jesse@htmlfusion.com>
2015-04-04 16:07:47 -0400

    Using ng-repeat to create into screens

--- client/app/intro/intro.controller.js
+++ client/app/intro/intro.controller.js
@@ -2,57 +2,22 @@
 
 angular.module('gishAppApp')
   .controller('IntroCtrl', function ($scope, $timeout) {
+
     $scope.intros = [{
-        /*caption: "Welcome"
+        caption: 'Welcome'
       },{
         caption: "You're going to have to move your feet"
       },{
-        caption: "The closer you get, the hotter you get"
+        caption: 'The closer you get, the hotter you get',
+        alignment: 'hot-text'
       },{
-        caption: "The further you get, the colder you get"
+        caption: 'The further you get, the colder you get',
+        alignment: 'cold-text'
       },{
-        caption: "Start moving!"
-      },{
-        caption: ""
-      },{
-        caption: {
-          earned: "You've earned the",
-          badge: "Explorer's Badge",
-          src: "app/intro/explorer-badge.png",
-          prize: "Found the prize",
-          points: "9000+ points",
-          replay: "Replay"   
-      }*/
-      
-      
-      
-      
-      
-      
-      caption: "Welcome"
-      },{
-        caption2: "You're going to have to move your feet"
-      },{
-        caption3: "The closer you get, the hotter you get"
-      },{
-        caption4: "The further you get, the colder you get"
-      },{
-        caption5: "Start moving!"
-      },{
-        caption6: ""
-      },{
-        caption7: {
-          earned: "You've earned the",
-          badge: "Explorer's Badge",
-          src: "app/intro/explorer-badge.png",
-          prize: "Found the prize",
-          points: "9000+ points",
-          replay: "Replay"   
+        caption: 'Start moving!'
       }
-    }];
-    
-    
-    $scope.user = {name: ''};
+    ];
+
   });
   
 
