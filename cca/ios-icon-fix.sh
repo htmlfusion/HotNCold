a751639fe75afa63d19dd9f75f82162f188f3729
Jesse Vander Does <jesse@htmlfusion.com>
2015-04-09 17:16:04 -0700

    Adding every possible icon size

---
+++ cca/ios-icon-fix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PROJNAME=Hot\ N\ Cold
+
+cp www/assets/icons/icon40.png "platforms/ios/$PROJNAME/Resources/icons/icon-40.png"
+cp www/assets/icons/icon80.png "platforms/ios/$PROJNAME/Resources/icons/icon-40@2x.png"
+cp www/assets/icons/icon120.png "platforms/ios/$PROJNAME/Resources/icons/icon-40@3x.png"
+cp www/assets/icons/icon50.png "platforms/ios/$PROJNAME/Resources/icons/icon-50.png"
+cp www/assets/icons/icon60.png "platforms/ios/$PROJNAME/Resources/icons/icon-60.png"
+cp www/assets/icons/icon120.png "platforms/ios/$PROJNAME/Resources/icons/icon-60@2x.png"
+cp www/assets/icons/icon180.png "platforms/ios/$PROJNAME/Resources/icons/icon-60@3x.png"
+cp www/assets/icons/icon72.png "platforms/ios/$PROJNAME/Resources/icons/icon-72.png"
+cp www/assets/icons/icon144.png "platforms/ios/$PROJNAME/Resources/icons/icon-72@2x.png"
+cp www/assets/icons/icon76.png "platforms/ios/$PROJNAME/Resources/icons/icon-76.png"
+cp www/assets/icons/icon152.png "platforms/ios/$PROJNAME/Resources/icons/icon-76@2x.png"
+cp www/assets/icons/icon29.png "platforms/ios/$PROJNAME/Resources/icons/icon-small.png"
+cp www/assets/icons/icon58.png "platforms/ios/$PROJNAME/Resources/icons/icon-small@2x.png"
+cp www/assets/icons/icon87.png "platforms/ios/$PROJNAME/Resources/icons/icon-small@3x.png"
+cp www/assets/icons/icon57.png "platforms/ios/$PROJNAME/Resources/icons/icon.png"
+cp www/assets/icons/icon114.png "platforms/ios/$PROJNAME/Resources/icons/icon@2x.png"
+
